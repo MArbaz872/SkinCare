6d6dd5aab212184b97d9ead753a8257e179d8b0f
MArbaz872 <101636839+MArbaz872@users.noreply.github.com>
2023-12-04 21:54:53 +0500

    report result

--- SkinCare/src/screen/BottomTabs/ResultScreen.js
+++ SkinCare/src/screen/BottomTabs/ResultScreen.js
@@ -6,17 +6,16 @@ import {
   View,
   ScrollView,
   Platform,
+  TouchableOpacity
 } from "react-native";
 import firebase from "firebase/compat";
-// import * as Print from "expo-print";
-// import { shareAsync } from "expo-sharing";
-// import { useNavigation } from '@react-navigation/native';
+import RNHTMLtoPDF from 'react-native-html-to-pdf';
+import Ionicons from "react-native-vector-icons/Ionicons";
+// import PDFView from 'react-native-view-pdf'; 
+import Share from 'react-native-share';
+import Spinner from 'react-native-loading-spinner-overlay';
 
 function ResultScreen(props) {
-  // const name3 = useNavigationParam('setingokay');
-  // const { setingokay } = route.params;
-  // console.log('setingokay', setingokay)
-  //   const { result } = route.params;
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [city, setCity] = useState("");
@@ -26,145 +25,234 @@ function ResultScreen(props) {
   const [phone, setPhone] = useState("");
   const [result, setResult] = useState("");
   const [image, setImage] = useState("");
-  
+  const [refresh, setRefresh] = useState(false);
   const user = firebase.auth().currentUser;
-  const html = `
-  <html lang="en">
-
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <title>Page Title</title>
-    <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <style type="text/css">
-  
-    </style>
-  </head>
-  
-  <body>
-    <div class="container-fluid">
-      <div class="row">
-        <div class="col-md-2"></div>
-        <div class="col-md-8">
-          <div class="card">
-            <div class="card-body">
-              <h4 class="card-title" style="color:#269A2A ;">Report</h4>
-              <div class="row" style="margin-top: 60px;">
-                <div class="col-md-4">
-                  <h6>Name:</h6>
-                  <p  autocomplete="off" name="name" placeholder="Type something" />
-                  ${name}
-                  </p>
-                </div>
-                <div class="col-4">
-                  
-                </div>
-                <div class="col-md-4">
-                  <h6>Email:</h6>
-                  <p  autocomplete="off" name="email" placeholder="Type something" />
-                  ${email}
-                  </p> 
-                </div>
-              </div>
-              <div class="row" style="margin-top: 60px;">
-                <div class="col-md-4">
-                  <h6>Address:</h6>
-                  <p autocomplete="off" name="address" placeholder="Type something" />
-                  ${city}
-                  </p> 
-                </div>
-                <div class="col-md-4">
-                  <h6>Contact:</h6>
-                  <p autocomplete="off" name="contact" placeholder="Type something" />
-                 ${phone}
-                  </p> 
-                </div>
-              </div>
-              <div class="row" style="margin-top: 60px;">
-                <div class="col-12">
-                  <h6>Result:</h6>
-                  <p autocomplete="off" name="address" placeholder="Type something" />	${result}
-                  </p>
-                </div>
-                <div class="col-12">
-                  <h6 style="color:#269A2A ;">Cure:</h6>
-                  <p autocomplete="off" name="contact" placeholder="Type something" />
-                  adasdas
-                  </p> 
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div class="col-md-2"></div>
-      </div>
-    </div>
-  </body>
-  
-  </html>
-`;
-  const [selectedPrinter, setSelectedPrinter] = React.useState();
-
-  const print = async () => {
-    // On iOS/android prints the given html. On web prints the HTML from the current page.
-    await Print.printAsync({
-      html,
-      printerUrl: selectedPrinter?.url, // iOS only
-    });
-  };
-
-  const printToFile = async () => {
-    // On iOS/android prints the given html. On web prints the HTML from the current page.
-    const { uri } = await Print.printToFileAsync({
-      html,
-    });
-    console.log("File has been saved to:", uri);
-    await shareAsync(uri, { UTI: ".pdf", mimeType: "application/pdf" });
-  };
-
-  const selectPrinter = async () => {
-    const printer = await Print.selectPrinterAsync(); // iOS only
-    setSelectedPrinter(printer);
-  };
+  const [pdfPath, setPdfPath] = useState(null);
+  const [spinner, setSpinner] = useState(false)
 
   useEffect(() => {
+
+      fetchData()
+     
+
+  }, [refresh]);
+  const resultParts = result.split(': ');
+  const condition = resultParts[0];
+  // console.log(condition, "fkdmfkm")
+  const refreshData = () => {
+   setSpinner(true)
+    // Call setRefresh to trigger a refresh
+    fetchData()
+    setRefresh(true);
+  };
+  const fetchData = () => {
     firebase
-      .firestore()
-      .collection("users")
-      .where("user_id", "==", user.uid)
-      .get()
-      .then((querySnapshot) => {
-        querySnapshot.forEach((documentSnapshot) => {
-          setName(documentSnapshot.data().name);
+    .firestore()
+    .collection("report")
+    .where("user_id", "==", user.uid)
+    .get()
+    .then((querySnapshot) => {
+      querySnapshot.forEach((documentSnapshot) => {
+        setName(documentSnapshot.data().name);
 
-          setEmail(documentSnapshot.data().email);
+        setEmail(documentSnapshot.data().email);
 
-          setAge(documentSnapshot.data().age);
-          setGender(documentSnapshot.data().gender);
+        setAge(documentSnapshot.data().age);
+        setGender(documentSnapshot.data().gender);
 
-          setCity(documentSnapshot.data().city);
+        setCity(documentSnapshot.data().city);
 
-          setCountry(documentSnapshot.data().country);
+        setCountry(documentSnapshot.data().country);
 
-          setPhone(documentSnapshot.data().phone);
+        setPhone(documentSnapshot.data().phone);
+        console.log('fsfsdfsd')
+        setResult(documentSnapshot.data().prediction);
+        setImage(props.route.params?.image);
+        setSpinner(false)
 
-          setResult(props.route.params?.result);
-          setImage(props.route.params?.image);
-          
+      }); });
+  };
+  const generatePDF = async () => {
+    setSpinner(true)
+    let options = {
+      html: `
+      <!DOCTYPE html>
+      <html>
+      <head>
+          <style>
+              body {
+                  font-family: Arial, sans-serif;
+                  background-color: #ffffff;
+                  margin: 0;
+                  padding: 0;
+              }
+      
+              header {
+                  color: #861E51;
+                  background-color: rgba(134,30,81,.20);
+                  padding: 10px;
+                  text-align: center;
+              }
+      
+              h1 {
+                  font-size: 24px;
+              }
+      
+              p {
+                  font-size: 16px;
+                  line-height: 1.5;
+                  margin: 20px;
+              }
+              .container {
+              }
+      
+              table {
+                  width: 100%;
+                  border-collapse: collapse;
+                  margin-top: 20px;
+              }
+      
+              th, td {
+                  padding: 10px;
+                  text-align: left;
+                  border-bottom: 1px solid #ddd;
+              }
+      
+              th {
+                  color: #000000;
+                  text-align: center;
+              }
+              /* styles.css */
+              .row-even {
+                  background-color: rgba(134, 30, 81, 0.1);
+              }
 
-        });
-        //console.log("Report", querySnapshot);
-      });
+              .row-odd {
+                  background-color: #ffffff;
+              }
 
-  }, []);
+          </style>
+      </head>
+      <body>
+          <header>
+              <h1>Report</h1>
+          </header>
+          <div  style="max-width: 800px; margin: 20px auto; padding: 20px; background-color: #ffffff; box-shadow: 0 10px 20px rgba(0, 0, 0, 0.4) border-radius: 30px; border: 1px solid #ddd;">
+              <table>
+                  <thead>
+                      <tr>
+                          <th>Report By :</th>
+                          <th> Skin Detact AI</th>
+                      </tr>
+                  </thead>
+                  <tbody>
+                  
+                  <tr class='row-odd' >
+                     <td>Name</td>
+                      <td>${name}</td>
+                    
+                  </tr>
+                  <tr class='row-even'>
+                     <td>Email</td>
+                      <td>${email}</td>
+                  </tr>
+                  <tr class='row-odd' >
+                  <td>City</td>
+                   <td>${city}</td>
+                 
+               </tr>
+               <tr class='row-even'>
+                  <td>Country</td>
+                   <td>${country}</td>
+               </tr>
+               <tr class='row-odd' >
+               <td>Age</td>
+                <td>${age}</td>
+              
+            </tr>
+            <tr class='row-even'>
+               <td>Gender</td>
+                <td>${gender}</td>
+            </tr>
+            <tr class='row-odd' >
+            <td>Phone No #</td>
+             <td>${phone}</td>
+           
+         </tr>
+         <tr class='row-even'>
+            <td>Result</td>
+             <td>${result}</td>
+         </tr>
+                  </tbody>
+              </table>
+          </div>
+      </body>
+      </html>
+      `,
+      fileName: 'SkinDetact'+condition,
+      directory: 'Phone',
+    };
 
-  console.log(props.route, "fkdmfkm")
+    let file = await RNHTMLtoPDF.convert(options)
+    try {
+      const shareOptions = {
+        title: 'Share via',
+        // message: 'Check out this PDF',
+        url: `file://${file.filePath}`,
+        type: 'application/pdf',
+      };
+
+      // setSpinner(false)
+      setTimeout(() => {
+        setSpinner(false)
+        Share.open(shareOptions)
+          .then((res) => {
+            console.log("yeh arha hai respons s  = = ", res);
+            if (res && res.action === Share.sharedAction) {
+              setSpinner(false)
+              console.log('Share Result:', res);
+            } else if (res && res.action === Share.dismissedAction) {
+              console.log('User canceled sharing');
+              setSpinner(false)
+            } else {
+              console.log('Share Result:', res);
+              setSpinner(false)
+            }
+          })
+          .catch((err) => {
+            console.log('Share Error:', err);
+            setSpinner(false)
+          });
+      }, 500)
+
+
+    } catch (error) {
+      setSpinner(false)
+      GeneralError(error.message, "Error!")
+      console.log('Error sharing PDF:', error.message);
+
+    }
+    console.log(file.filePath);
+  };
   return (
     <>
       <View style={styles.container}>
+      {/* {pdfPath && (
+          // Render PDFView component if pdfPath is available
+          <PDFView
+            fadeInDuration={250.0}
+            style={{ flex: 1 }}
+            resource={pdfPath}
+            resourceType="file"
+            onLoad={() => console.log(`PDF rendered from ${pdfPath}`)}
+          />
+        )} */}
+        <View style={{width:'60%',flexDirection:'row'}}>
         <Text style={styles.title}>Report</Text>
+        <TouchableOpacity style={{top:20,left:10}} onPress={refreshData}>
+              <Ionicons name="reload" size={20}  />
+            </TouchableOpacity>
+            </View>
         <View style={[styles.colView, { justifyContent: "space-between" }]}>
           <View>
             <Text style={styles.fieldTitle}>NAME:</Text>
@@ -197,19 +285,19 @@ function ResultScreen(props) {
           <View style={{ flexDirection: "row", marginTop: 20 }}>
             <Text style={styles.fieldTitle}>Cure: </Text>
             {/* 'Acne', 'Eczema', 'Healthy', 'Psoriasis' */}
-            {result == "Acne" ? (
+            {condition == "Acne" ? (
               <Text style={styles.field}>
                 Medications: Isotretion Cream. Clindamycin Plus Tetrinoin. Apply
                 to affected areas once or twice daily or as directed by a
                 dermatologist
               </Text>
-            ) : result == "Eczema" ? (
+            ) : condition == "Eczema" ? (
               <Text style={styles.field}>
                 Medications: Hydrophil Lotion. Hydrocortisone Cream.Apply to
                 affected areas once or twice daily or as directed by a
                 dermatologist
               </Text>
-            ) : result == "Psoriasis" ? (
+            ) : condition == "Psoriasis" ? (
               <Text style={styles.field}>
                 Medications: Topical Steroids Cream. Hydrocortisone.Apply to
                 affected areas once or twice daily or as directed by a
@@ -221,6 +309,7 @@ function ResultScreen(props) {
           </View>
         </View>
       </View>
+     
       <View
         style={[
           {
@@ -231,10 +320,17 @@ function ResultScreen(props) {
           },
         ]}
       >
-        <Button title="Download" color="#05375a" onPress={() => print()} />
+         <Spinner
+        visible={spinner}
+        // textContent={'Generating PDF...'}
+        textStyle={{ color:'#05375a', fontFamily:'semiBold' }}
+
+      />
+        <Button title="Download" color="#05375a" onPress={generatePDF} />
+        {/* <Button title="Refresh" color="#05375a"  /> */}
       </View>
-    </>
-  );
+      </>
+  )
 }
 
 export default ResultScreen;
@@ -251,6 +347,9 @@ const styles = StyleSheet.create({
     // width:"100%",
     borderBottomColor: "black",
     borderBottomWidth: 1,
+    justifyContent:'center',
+    // backgroundColor:'pink',
+    right:5
   },
   title: {
     fontSize: 36,
